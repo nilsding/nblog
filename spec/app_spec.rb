1f0de4633ad93ef3266b09f46af464284a39eeac
nilsding <nilsding@nilsding.org>
2014-10-11 00:08:02 +0200

    added new spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,12 +2,24 @@ require File.expand_path "../spec_helper.rb", __FILE__
 
 describe "nblog" do
   it "should allow accessing the home page" do
-    get "/"
-    expect(last_response).to be_ok
+    visit "/"
+    expect(page.status_code).to be(200)
   end
+  
   it "should not be able to access the logout page via GET" do
-    get "/logout"
-    expect(last_response).not_to be_ok
+    visit "/logout"
+    expect(page.status_code).to be(403)
+  end
+  
+  it "should set a user-defined stylesheet" do
+    cssfile = "http://test.nilsding.org/nblog2.css"
+    
+    visit "/?css=%20%20%20%20#{cssfile}%20%20%20%20"
+    expect(page.status_code).to be(200)
+    expect(page).to have_xpath("//link[@rel='stylesheet' and @href='#{cssfile}']")
+    
+    visit "/?css=%20%20%20%20"
+    expect(page).to have_xpath("//link[@rel='stylesheet' and @href='/assets/style.css']")
   end
 end
 
