a178ed75c886511536a661b3513eb6738f5c0565
nilsding <nilsding@nilsding.org>
2014-10-11 10:02:32 +0200

    now using in-memory database for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
 require "rspec"
 require "capybara/rspec"
 
-require File.expand_path "../../lib/application", __FILE__
-
 ENV['RACK_ENV'] = 'test'
+
+require File.expand_path "../../lib/application", __FILE__
+require File.expand_path "../../lib/interactiveshell", __FILE__
+
+s = NBlog::InteractiveShell.new [], false
+s.parse "init"
+s.parse "add-user test secret"
 
 Capybara.app = NBlog::Application
 Capybara.ignore_hidden_elements = false
