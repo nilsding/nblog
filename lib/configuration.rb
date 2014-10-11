4bc0740c12f9a484962ffc0b1687f891f642bf5d
nilsding <nilsding@nilsding.org>
2014-10-11 09:59:57 +0200

    now using :memory: database for testing

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -19,6 +19,6 @@ module NBlog
     @@config ||= YAML.load_file File.expand_path(".", "config.yml")
 
     # SQLite3 database connection
-    @@db ||= SQLite3::Database.new File.expand_path(".", @@config['dbfile'])
+    @@db ||= SQLite3::Database.new ENV['RACK_ENV'] == "test" ? ":memory:" : File.expand_path(".", @@config['dbfile'])
     # TODO: check if database was initialized
 end
